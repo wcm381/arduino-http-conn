398a4f9206ac4e26efaeb2ca50ca0534cad5a309
wcm381 <wcm381@gmail.com>
2014-07-14 00:15:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,37 +9,38 @@ var http = require('http'),
     server;
 
 var remoteClients = [];
+var dataVal = 0;
+var obj;
 
 server = http.createServer( function(req,res){
-  var path = url.parse(req.url).pathname;
-  if (req.method == 'POST') {
-      console.log("POST");
-      console.log(req.headers);
-      
-      var body = "";
-      req.on('data', function(data) {
-            console.log("Data length: " + data.length);
-            body += data;
-            console.log("Partial body: " + data);
-      });
-      req.on('end', function () {
-          console.log("Body: " + body);
-          var obj = JSON.parse(body);
-          console.log(obj.data);
-      });
-  }
-});
-server.listen(HTTP_PORT);
-  
-  
-  
-  
+   var path = url.parse(req.url).pathname;
 
- /* 
-  switch (path){
+   var timeVal = getDataTime();
+   switch (path){
     case '/':
+      if (req.method == 'POST') {
+        console.log("POST");
+        console.log(req.headers);
+          
+        var body = "";
+        req.on('data', function(data) {
+            body += data;
+        });
+        req.on('end', function () {
+                console.log("Body: " + body);
+                var obj = JSON.parse(body);
+                console.log(obj.data);
+        });
+      } else {
+        console.log(req.method);
+      }
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<h1>Welcome.</h1>');
+      res.write('<html><head?<script scr="/socket.io/socket.io.js"></script>');
+      res.write('<title>Hello</title></head><body>');
+      res.write('<h1>'+dataVal+'</h1>')
+      res.write('<p>'+timeVal+'</p>')
+      res.write('<script>' + 'var socket = io.connect();' + '</script>');
+      res.write('</body></html>');
       res.end();
       break;
   default: send404(res);
@@ -50,38 +51,48 @@ send404 = function(res){
   res.writeHead(404);
   res.write('404');
   res.end();
-};
-
-
-
-net.createServer(function(socket){
-    
-  console.log('Connected Remote Client: ' + socket.remoteAddress +':'+ socket.remotePort);
-  remoteClients.push(socket); //add client
+}; 
   
   
-  console.log('tcp server running on port ', REMOTE_PORT);
-  console.log('web server running on port ', HTTP_PORT);
+});
+server.listen(HTTP_PORT);
   
-  socket.on('close', function() {
-        for(var i = 0; i < remoteClients.length; i++) {
-            if(remoteClients[i] == socket) { //remove client
-                remoteClients.splice(i,1);
-                break;
-            }
-        }
-    console.log('Disconnected remote client: ' + socket.remoteAddress +':'+ socket.remotePort);
-    });
-    // Add a 'data' event handler to this instance of socket
-    socket.on('data', function(data) {
-        console.log('data?');
-        for ( var i = 0; i < data.length; i++){
-            handleByte(data[i]);
-        }
-        function handleByte(buf){}
-    });
-}).listen(REMOTE_PORT);
-console.log('Server listening for remote connections on ' + REMOTE_PORT);
 
-*/
+
+var io = io.listent(server);
+
+io.on('connection', function(client) {
+        console.log('Connected!');
+        
+        client.on('message', function(message){
+            console.log('msg obj', message);
+        });
+});
+
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
+
 
